3a9873416bec9b642a428f1ce2408cde5a18d4ed
Gordon Tu <tugordon@outlook.comqeixtexitgit config --global user.email tugordon@outlook.com>
2020-01-21 10:59:19 -0500

    d3

---
+++ D3_map_animation/app.js
@@ -0,0 +1,39 @@
+async function drawmap(){
+
+      const countryShapes = await d3.json('./../world-geojson.json')
+
+      const dataset = await d3.csv("../country_year_temp.csv")
+//*////////////////?       /////////////////////////////////////////////////////////////////////////////////
+//!////////////? Data Manipulation//////////////////////////////////////////////////////////////////////////
+//*////////////////?       /////////////////////////////////////////////////////////////////////////////////
+
+      //TODO we want an easy to look up forest using country names
+      const countryNameAccessor = d => d.properties["NAME"]
+      const countryIdAccessor = d => d.properties["ADM0_A3_IS"]
+     // const metric = "20"
+
+      let metricDataByCountry = {}
+      
+      dataset.forEach(d => {
+            if (d['Year'] = 2000)
+            return metricDataByCountry[d['Country']] = +d['Mean_temp_per_year'] || 0
+      })  // making an object
+//*////////////////?       /////////////////////////////////////////////////////////////////////////////////
+//!////////////? Define Dimensions /////////////////////////////////////////////////////////////////////////
+//*////////////////?       /////////////////////////////////////////////////////////////////////////////////
+
+      let dimensions ={
+            width:window.innerWidth*0.9,
+            margin:{
+                  top:10, right:10, bottom:10, left:10
+            }
+      }
+      dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+
+
+
+      }
+
+      
+
+      drawmap()
